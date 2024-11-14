ee9c3564c4cdefb45327e7192e826f2a0a447230
Thaina-ADS <thainarodriguesmiranda@gmail.com>
2024-11-14 13:49:40 -0300

    area de login

---
+++ app/src/js/index.js
@@ -0,0 +1,24 @@
+/* 
+Campos dos formulários para enviar para o Banco de Dados
+
+Campos: Nome, E-mail, Telefone, Celular, Faturamento Mensal.
+
+1- Chamar função salvarFormulario que vai pegar os dados digitados e formar um objeto no seguinte formato
+    {nome: "", email: "", telefone: "", celular: "", faturamentoMensal: ""}
+
+2- A função salvarFormulario deve chamar a função validarCampos que fará a validação dos campos
+digitados pelo usuário, verificando se não estão preenchidos, se 
+eles estiverem preenchidos, então deve ser dado alert("Dados salvos com sucesso!") 
+OBS.: Estudar requisições ajax com js
+
+*/
+
+function salvarFormulario() {
+   // pegar valor dos campos
+
+   // validar se os campos estao preenchidos
+
+   // se os campos não estiverem preenchidos, alert("Preencha o campo obrigatório")
+
+   // se os campos estiverem preenchidos, alert("Salvo com sucesso")
+}
