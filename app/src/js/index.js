5a9a417ecfc593a39d8f67f745c7e33d083c98cd
Thaina-ADS <thainarodriguesmiranda@gmail.com>
2025-01-23 12:06:27 -0300

    comentando index.js

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -16,28 +16,29 @@ OBS.: Estudar requisições ajax com js
 const form = document.querySelector('#formContato')
 
 // Adiciona evento de 'submit' ao formulário
-form.addEventListener('submit', (event) => {
 
-  // Impede o envio tradicional do formulário
-  event.preventDefault();
+// form.addEventListener('submit', (event) => {
 
-  // Obtém dados do formulário e converte para objeto Javascript
-  const data = Object.fromEntries(new FormData(event.target).entries());
-  console.log(data);
+//   // Impede o envio tradicional do formulário
+//   event.preventDefault();
 
-  var nome = data.nome;
-  var email = data.email;
-  var celular = data.celular;
+//   // Obtém dados do formulário e converte para objeto Javascript
+//   const data = Object.fromEntries(new FormData(event.target).entries());
+//   console.log(data);
 
-  validarFormulario(nome, email, celular);
-});
+//   var nome = data.nome;
+//   var email = data.email;
+//   var celular = data.celular;
+
+//   validarFormulario(nome, email, celular);
+// });
 
 function valideNome(nome) {
     var nomePattern = /^[a-zA-ZÀ-ÿ]+(?:[\s-][a-zA-ZÀ-ÿ]+)*$/;
     if (nome === "" || nome.length < 3) {
         Notify("Preencha o campo de nome com pelo menos 3 caracteres", null, null, "warning");
         return false;
-    }else if (!nomePattern.test(nome)) {
+    } else if (!nomePattern.test(nome)) {
         Notify("Por favor, insira um nome válido.", null, null, "warning");
         return false;
     }
@@ -49,7 +50,7 @@ function valideEmail(email) {
     if (email === "") {
         Notify("Preencha o campo de e-mail", null, null, "warning");
         return false;
-    }else if (!emailPattern.test(email)) {
+    } else if (!emailPattern.test(email)) {
         Notify("Por favor, insira um e-mail válido.", null, null, "warning");
         return false;
     }
@@ -66,28 +67,28 @@ function valideCelular(celular) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
-const phoneInput = document.querySelectorAll(".phone");
+document.addEventListener("DOMContentLoaded", function () {
+    const phoneInput = document.querySelectorAll(".phone");
 
 
- // Aplica a máscara a cada um dos inputs
- phoneInput.forEach(function(inputPhone) {
-   inputPhone.addEventListener("input", function(event) {
-     let value = event.target.value;
-     value = value.replace(/\D/g, ""); // Remove tudo o que não é número
-     value = value.substring(0, 11); // Limita a 11 dígitos
+    // Aplica a máscara a cada um dos inputs
+    phoneInput.forEach(function (inputPhone) {
+        inputPhone.addEventListener("input", function (event) {
+            let value = event.target.value;
+            value = value.replace(/\D/g, ""); // Remove tudo o que não é número
+            value = value.substring(0, 11); // Limita a 11 dígitos
 
-     // Formata o número conforme os caracteres são digitados
-     if (value.length > 2 && value.length <= 7) {
-       value = `(${value.substring(0, 2)}) ${value.substring(2)}`;
-     } else if (value.length > 7) {
-       value = `(${value.substring(0, 2)}) ${value.substring(2, 7)}-${value.substring(7)}`;
-     }
-     console.log(value);
-     console.log(event.target.value);
-     inputPhone.value = value
+            // Formata o número conforme os caracteres são digitados
+            if (value.length > 2 && value.length <= 7) {
+                value = `(${value.substring(0, 2)}) ${value.substring(2)}`;
+            } else if (value.length > 7) {
+                value = `(${value.substring(0, 2)}) ${value.substring(2, 7)}-${value.substring(7)}`;
+            }
+            console.log(value);
+            console.log(event.target.value);
+            inputPhone.value = value
+        })
     })
-})
 })
 
 function validarFormulario(nome, email, celular) {
@@ -95,7 +96,7 @@ function validarFormulario(nome, email, celular) {
     var emailIsValid = valideEmail(email);
     var celularIsValid = valideCelular(celular);
 
-    if(nomeIsValid && emailIsValid && celularIsValid) {
+    if (nomeIsValid && emailIsValid && celularIsValid) {
         Notify("Formulário realizado com sucesso!", null, null, "success");
     }
 }
